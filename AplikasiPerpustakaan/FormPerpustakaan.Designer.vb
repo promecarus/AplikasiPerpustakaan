8f33d0784f0c0820d14151e089484cf25e976539
promecarus <haikalslipi@gmail.com>
2022-11-01 20:17:09 +0700

    Slide 5 safe

--- AplikasiPerpustakaan/FormPerpustakaan.Designer.vb
+++ AplikasiPerpustakaan/FormPerpustakaan.Designer.vb
@@ -22,16 +22,104 @@ Partial Class FormPerpustakaan
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormPerpustakaan))
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.KoleksiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TambahKoleksiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStrip = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButtonPlus = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButtonMinus = New System.Windows.Forms.ToolStripButton()
+        Me.ListBoxKoleksi = New System.Windows.Forms.ListBox()
+        Me.MenuStrip.SuspendLayout()
+        Me.ToolStrip.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Form1
+        'MenuStrip
+        '
+        Me.MenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.KoleksiToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(382, 28)
+        Me.MenuStrip.TabIndex = 0
+        Me.MenuStrip.Text = "MenuStrip1"
+        '
+        'KoleksiToolStripMenuItem
+        '
+        Me.KoleksiToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TambahKoleksiToolStripMenuItem})
+        Me.KoleksiToolStripMenuItem.Name = "KoleksiToolStripMenuItem"
+        Me.KoleksiToolStripMenuItem.Size = New System.Drawing.Size(70, 24)
+        Me.KoleksiToolStripMenuItem.Text = "Koleksi"
+        '
+        'TambahKoleksiToolStripMenuItem
+        '
+        Me.TambahKoleksiToolStripMenuItem.Name = "TambahKoleksiToolStripMenuItem"
+        Me.TambahKoleksiToolStripMenuItem.Size = New System.Drawing.Size(195, 26)
+        Me.TambahKoleksiToolStripMenuItem.Text = "Tambah Koleksi"
+        '
+        'ToolStrip
+        '
+        Me.ToolStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ToolStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButtonPlus, Me.ToolStripButtonMinus})
+        Me.ToolStrip.Location = New System.Drawing.Point(0, 28)
+        Me.ToolStrip.Name = "ToolStrip"
+        Me.ToolStrip.Size = New System.Drawing.Size(382, 27)
+        Me.ToolStrip.TabIndex = 1
+        Me.ToolStrip.Text = "ToolStrip1"
+        '
+        'ToolStripButtonPlus
+        '
+        Me.ToolStripButtonPlus.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButtonPlus.Image = CType(resources.GetObject("ToolStripButtonPlus.Image"), System.Drawing.Image)
+        Me.ToolStripButtonPlus.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButtonPlus.Name = "ToolStripButtonPlus"
+        Me.ToolStripButtonPlus.Size = New System.Drawing.Size(29, 24)
+        Me.ToolStripButtonPlus.Text = "Form Tambah Koleksi"
+        '
+        'ToolStripButtonMinus
+        '
+        Me.ToolStripButtonMinus.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButtonMinus.Image = CType(resources.GetObject("ToolStripButtonMinus.Image"), System.Drawing.Image)
+        Me.ToolStripButtonMinus.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButtonMinus.Name = "ToolStripButtonMinus"
+        Me.ToolStripButtonMinus.Size = New System.Drawing.Size(29, 24)
+        Me.ToolStripButtonMinus.Text = "Form Hapus Koleksi"
+        '
+        'ListBoxKoleksi
+        '
+        Me.ListBoxKoleksi.FormattingEnabled = True
+        Me.ListBoxKoleksi.ItemHeight = 20
+        Me.ListBoxKoleksi.Location = New System.Drawing.Point(12, 58)
+        Me.ListBoxKoleksi.Name = "ListBoxKoleksi"
+        Me.ListBoxKoleksi.Size = New System.Drawing.Size(358, 384)
+        Me.ListBoxKoleksi.TabIndex = 3
+        '
+        'FormPerpustakaan
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(382, 454)
+        Me.Controls.Add(Me.ListBoxKoleksi)
+        Me.Controls.Add(Me.ToolStrip)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.Name = "FormPerpustakaan"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Perpustakaan"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.ToolStrip.ResumeLayout(False)
+        Me.ToolStrip.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents MenuStrip As MenuStrip
+    Friend WithEvents KoleksiToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TambahKoleksiToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStrip As ToolStrip
+    Friend WithEvents ToolStripButtonPlus As ToolStripButton
+    Friend WithEvents ToolStripButtonMinus As ToolStripButton
+    Friend WithEvents ListBoxKoleksi As ListBox
 End Class
--- AplikasiPerpustakaan/FormPerpustakaan.vb
+++ AplikasiPerpustakaan/FormPerpustakaan.vb
@@ -1,3 +1,16 @@
 ﻿Public Class FormPerpustakaan
+    Private Sub TambahKoleksiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TambahKoleksiToolStripMenuItem.Click
+        FormTambahKoleksi.Show()
+        'Me.Hide()
+    End Sub
 
+    Private Sub ToolStripButtonPlus_Click(sender As Object, e As EventArgs) Handles ToolStripButtonPlus.Click
+        FormTambahKoleksi.Show()
+        'Me.Hide()
+    End Sub
+
+    Private Sub ToolStripButtonMinus_Click(sender As Object, e As EventArgs) Handles ToolStripButtonMinus.Click
+        FormHapusKoleksi.Show()
+        'Me.Hide()
+    End Sub
 End Class
