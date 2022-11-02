f7c4cad2790268c600e2ec3e598d843fbac82660
promecarus <haikalslipi@gmail.com>
2022-11-02 11:09:27 +0700

    Update FormHapusKoleksi.vb

--- AplikasiPerpustakaan/FormHapusKoleksi.vb
+++ AplikasiPerpustakaan/FormHapusKoleksi.vb
@@ -1,3 +1,8 @@
 ﻿Public Class FormHapusKoleksi
-
+    Private Sub ButtonHapus_Click(sender As Object, e As EventArgs) Handles ButtonHapus.Click
+        'MessageBox.Show(LabelKoleksi.Text.ToString)
+        FormPerpustakaan.ListBoxKoleksi.Items.Remove(LabelKoleksi.Text.ToString)
+        Me.Close()
+        FormPerpustakaan.Show()
+    End Sub
 End Class
