f19b30f0be2d2149e84444379152869909d5cc7e
promecarus <haikalslipi@gmail.com>
2022-11-02 11:09:44 +0700

    Update FormPerpustakaan.vb

--- AplikasiPerpustakaan/FormPerpustakaan.vb
+++ AplikasiPerpustakaan/FormPerpustakaan.vb
@@ -1,19 +1,35 @@
 ﻿Public Class FormPerpustakaan
+    Public Shared dataKoleksi As ClassKoleksi
+    Public Shared listDataKoleksi As List(Of String)
+
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        dataKoleksi = New ClassKoleksi
+    End Sub
+
+    Public Sub tambahKoleksi(nama As String)
+        ListBoxKoleksi.Items.Add(nama)
+    End Sub
+
     Private Sub TambahKoleksiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TambahKoleksiToolStripMenuItem.Click
         FormTambahKoleksi.Show()
-        'Me.Hide()
+        Me.Hide()
     End Sub
 
     Private Sub ToolStripButtonPlus_Click(sender As Object, e As EventArgs) Handles ToolStripButtonPlus.Click
         FormTambahKoleksi.Show()
-        'Me.Hide()
+        Me.Hide()
     End Sub
 
     Private Sub ToolStripButtonMinus_Click(sender As Object, e As EventArgs) Handles ToolStripButtonMinus.Click
         If String.IsNullOrEmpty(ListBoxKoleksi.SelectedItem) Then
             MessageBox.Show("Pilih koleksi yang ingin dihapus")
         Else
-            FormHapusKoleksi.LabelKonfirmasi.Text += ListBoxKoleksi.SelectedItem.ToString + "?"
+            FormHapusKoleksi.LabelKoleksi.Text = ListBoxKoleksi.SelectedItem.ToString
+            Me.Hide()
             FormHapusKoleksi.Show()
         End If
     End Sub
