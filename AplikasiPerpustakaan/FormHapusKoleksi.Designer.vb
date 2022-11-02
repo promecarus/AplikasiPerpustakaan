0fff2f375ac98587ba0a3393e4ad85492fc1c7f0
promecarus <haikalslipi@gmail.com>
2022-11-02 11:09:23 +0700

    Update FormHapusKoleksi.Designer.vb

--- AplikasiPerpustakaan/FormHapusKoleksi.Designer.vb
+++ AplikasiPerpustakaan/FormHapusKoleksi.Designer.vb
@@ -24,6 +24,7 @@ Partial Class FormHapusKoleksi
     Private Sub InitializeComponent()
         Me.LabelKonfirmasi = New System.Windows.Forms.Label()
         Me.ButtonHapus = New System.Windows.Forms.Button()
+        Me.LabelKoleksi = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'LabelKonfirmasi
@@ -31,7 +32,7 @@ Partial Class FormHapusKoleksi
         Me.LabelKonfirmasi.Dock = System.Windows.Forms.DockStyle.Top
         Me.LabelKonfirmasi.Location = New System.Drawing.Point(0, 0)
         Me.LabelKonfirmasi.Name = "LabelKonfirmasi"
-        Me.LabelKonfirmasi.Size = New System.Drawing.Size(298, 106)
+        Me.LabelKonfirmasi.Size = New System.Drawing.Size(346, 54)
         Me.LabelKonfirmasi.TabIndex = 0
         Me.LabelKonfirmasi.Text = "Apakah ingin menghapus "
         Me.LabelKonfirmasi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -39,18 +40,31 @@ Partial Class FormHapusKoleksi
         'ButtonHapus
         '
         Me.ButtonHapus.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.ButtonHapus.Location = New System.Drawing.Point(0, 121)
+        Me.ButtonHapus.Location = New System.Drawing.Point(0, 132)
         Me.ButtonHapus.Name = "ButtonHapus"
-        Me.ButtonHapus.Size = New System.Drawing.Size(298, 37)
+        Me.ButtonHapus.Size = New System.Drawing.Size(346, 37)
         Me.ButtonHapus.TabIndex = 1
         Me.ButtonHapus.Text = "Hapus"
         Me.ButtonHapus.UseVisualStyleBackColor = True
+        '
+        'LabelKoleksi
+        '
+        Me.LabelKoleksi.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LabelKoleksi.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.LabelKoleksi.ForeColor = System.Drawing.Color.IndianRed
+        Me.LabelKoleksi.Location = New System.Drawing.Point(0, 54)
+        Me.LabelKoleksi.Name = "LabelKoleksi"
+        Me.LabelKoleksi.Size = New System.Drawing.Size(346, 78)
+        Me.LabelKoleksi.TabIndex = 2
+        Me.LabelKoleksi.Text = "Koleksi"
+        Me.LabelKoleksi.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'FormHapusKoleksi
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(298, 158)
+        Me.ClientSize = New System.Drawing.Size(346, 169)
+        Me.Controls.Add(Me.LabelKoleksi)
         Me.Controls.Add(Me.ButtonHapus)
         Me.Controls.Add(Me.LabelKonfirmasi)
         Me.Name = "FormHapusKoleksi"
@@ -62,4 +76,5 @@ Partial Class FormHapusKoleksi
 
     Friend WithEvents LabelKonfirmasi As Label
     Friend WithEvents ButtonHapus As Button
+    Friend WithEvents LabelKoleksi As Label
 End Class
