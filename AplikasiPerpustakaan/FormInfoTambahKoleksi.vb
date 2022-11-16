fb4ace823a925b703eead56834b4a702cc15a4ce
promecarus <haikalslipi@gmail.com>
2022-11-16 21:47:56 +0700

    Update FormInfoTambahKoleksi.vb

--- AplikasiPerpustakaan/FormInfoTambahKoleksi.vb
+++ AplikasiPerpustakaan/FormInfoTambahKoleksi.vb
@@ -4,14 +4,14 @@
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
-        If Not String.IsNullOrEmpty(FormPerpustakaan.dataKoleksi.GSGambarKoleksi) Then
-            PictureBoxInfoGambarKoleksi.Load(FormPerpustakaan.dataKoleksi.GSGambarKoleksi)
+        If Not String.IsNullOrEmpty(FormPerpustakaan.dataKoleksi.GSDirGambarBuku) Then
+            PictureBoxInfoGambarKoleksi.Load(FormPerpustakaan.dataKoleksi.GSDirGambarBuku)
             PictureBoxInfoGambarKoleksi.SizeMode = PictureBoxSizeMode.StretchImage
         End If
 
         LabelInfoNamaKoleksi.Text = FormPerpustakaan.dataKoleksi.GSNamaKoleksi
         LabelInfoJenisKoleksi.Text = FormPerpustakaan.dataKoleksi.GSJenisKoleksi
-        LabelInfoDeskripsi.Text = FormPerpustakaan.dataKoleksi.GSDeskripsi
+        LabelInfoDeskripsi.Text = FormPerpustakaan.dataKoleksi.GSDeskripsiKoleksi
         LabelInfoPenerbit.Text = FormPerpustakaan.dataKoleksi.GSPenerbit
 
         If FormPerpustakaan.dataKoleksi.GSTahunTerbit.ToString IsNot "0" Then
@@ -20,7 +20,7 @@
             LabelInfoTahunTerbit.Text = "Tahun terbit belum diset"
         End If
 
-        LabelInfoLokasiRak.Text = FormPerpustakaan.dataKoleksi.GSLokasiRak
+        LabelInfoLokasiRak.Text = FormPerpustakaan.dataKoleksi.GSLokasi
         LabelInfoTanggalMasukKoleksi.Text = FormPerpustakaan.dataKoleksi.GSTanggalMasukKoleksi
 
         If FormPerpustakaan.dataKoleksi.GSStock.ToString IsNot "0" Then
@@ -34,11 +34,11 @@
 
         ListBoxInfoKategori.Items.Clear()
 
-        For Each kategori In FormPerpustakaan.dataKoleksi.GSKategoriList
+        For Each kategori In FormPerpustakaan.dataKoleksi.GSKategori
             ListBoxInfoKategori.Items.Add(kategori)
         Next
 
-        FormPerpustakaan.dataKoleksi.GSKategoriList.Clear()
+        FormPerpustakaan.dataKoleksi.GSKategori.Clear()
     End Sub
 
     Private Sub ButtonTambahKoleksi_Click(sender As Object, e As EventArgs) Handles ButtonTambahKoleksi.Click
