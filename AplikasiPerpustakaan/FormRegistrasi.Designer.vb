756073e99fe2cc8109cf94f5ec37e311b8ba26dd
promecarus <haikalslipi@gmail.com>
2022-11-27 19:54:09 +0700

    form registrasi

---
+++ AplikasiPerpustakaan/FormRegistrasi.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormRegistrasi
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonRegistrasi = New System.Windows.Forms.Button()
+        Me.TextBoxPassword = New System.Windows.Forms.TextBox()
+        Me.TextBoxUsername = New System.Windows.Forms.TextBox()
+        Me.LabelPassword = New System.Windows.Forms.Label()
+        Me.LabelUsername = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ButtonRegistrasi
+        '
+        Me.ButtonRegistrasi.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ButtonRegistrasi.Location = New System.Drawing.Point(0, 174)
+        Me.ButtonRegistrasi.Name = "ButtonRegistrasi"
+        Me.ButtonRegistrasi.Size = New System.Drawing.Size(320, 29)
+        Me.ButtonRegistrasi.TabIndex = 7
+        Me.ButtonRegistrasi.Text = "Registrasi"
+        Me.ButtonRegistrasi.UseVisualStyleBackColor = True
+        '
+        'TextBoxPassword
+        '
+        Me.TextBoxPassword.Location = New System.Drawing.Point(150, 100)
+        Me.TextBoxPassword.Name = "TextBoxPassword"
+        Me.TextBoxPassword.Size = New System.Drawing.Size(125, 27)
+        Me.TextBoxPassword.TabIndex = 5
+        '
+        'TextBoxUsername
+        '
+        Me.TextBoxUsername.Location = New System.Drawing.Point(150, 50)
+        Me.TextBoxUsername.Name = "TextBoxUsername"
+        Me.TextBoxUsername.Size = New System.Drawing.Size(125, 27)
+        Me.TextBoxUsername.TabIndex = 3
+        '
+        'LabelPassword
+        '
+        Me.LabelPassword.AutoSize = True
+        Me.LabelPassword.Location = New System.Drawing.Point(50, 100)
+        Me.LabelPassword.Name = "LabelPassword"
+        Me.LabelPassword.Size = New System.Drawing.Size(70, 20)
+        Me.LabelPassword.TabIndex = 6
+        Me.LabelPassword.Text = "Password"
+        '
+        'LabelUsername
+        '
+        Me.LabelUsername.AutoSize = True
+        Me.LabelUsername.Location = New System.Drawing.Point(50, 50)
+        Me.LabelUsername.Name = "LabelUsername"
+        Me.LabelUsername.Size = New System.Drawing.Size(75, 20)
+        Me.LabelUsername.TabIndex = 4
+        Me.LabelUsername.Text = "Username"
+        '
+        'FormRegistrasi
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(320, 203)
+        Me.Controls.Add(Me.ButtonRegistrasi)
+        Me.Controls.Add(Me.TextBoxPassword)
+        Me.Controls.Add(Me.TextBoxUsername)
+        Me.Controls.Add(Me.LabelPassword)
+        Me.Controls.Add(Me.LabelUsername)
+        Me.Name = "FormRegistrasi"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Registrasi"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonRegistrasi As Button
+    Friend WithEvents TextBoxPassword As TextBox
+    Friend WithEvents TextBoxUsername As TextBox
+    Friend WithEvents LabelPassword As Label
+    Friend WithEvents LabelUsername As Label
+End Class
---
+++ AplikasiPerpustakaan/FormRegistrasi.vb
@@ -0,0 +1,11 @@
+﻿Public Class FormRegistrasi
+    Private Sub ButtonRegistrasi_Click(sender As Object, e As EventArgs) Handles ButtonRegistrasi.Click
+        FormLogin.Users.AddAuth(TextBoxUsername.Text, TextBoxPassword.Text)
+        Me.Close()
+        FormLogin.Show()
+    End Sub
+
+    Private Sub FormRegistrasi_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        FormLogin.Show()
+    End Sub
+End Class
