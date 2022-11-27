b1edc68f5c80084b24278f76c22d27e4d9bf4754
promecarus <haikalslipi@gmail.com>
2022-11-27 15:47:41 +0700

    Update FormLogin.vb

--- AplikasiPerpustakaan/FormLogin.vb
+++ AplikasiPerpustakaan/FormLogin.vb
@@ -1,3 +1,16 @@
 ﻿Public Class FormLogin
+    Dim Users As New ClassUsers
 
+    Private Sub ButtonLogin_Click(sender As Object, e As EventArgs) Handles ButtonLogin.Click
+        Dim plainUsername As String = TextBoxUsername.Text
+        Dim plainPassword As String = TextBoxPassword.Text
+
+        Dim AuthStatus As Boolean = Users.CheckAuth(plainUsername, plainPassword)
+
+        If AuthStatus Then
+            FormPerpustakaan.Show()
+        Else
+            MessageBox.Show("Wrong Password")
+        End If
+    End Sub
 End Class
