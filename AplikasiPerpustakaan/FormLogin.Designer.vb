25002866d51f50fb49c5f212363f8cebbf31cdeb
promecarus <haikalslipi@gmail.com>
2022-11-27 15:47:38 +0700

    Update FormLogin.Designer.vb

--- AplikasiPerpustakaan/FormLogin.Designer.vb
+++ AplikasiPerpustakaan/FormLogin.Designer.vb
@@ -22,9 +22,75 @@ Partial Class FormLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.LabelUsername = New System.Windows.Forms.Label()
+        Me.LabelPassword = New System.Windows.Forms.Label()
+        Me.TextBoxUsername = New System.Windows.Forms.TextBox()
+        Me.TextBoxPassword = New System.Windows.Forms.TextBox()
+        Me.ButtonLogin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LabelUsername
+        '
+        Me.LabelUsername.AutoSize = True
+        Me.LabelUsername.Location = New System.Drawing.Point(50, 50)
+        Me.LabelUsername.Name = "LabelUsername"
+        Me.LabelUsername.Size = New System.Drawing.Size(75, 20)
+        Me.LabelUsername.TabIndex = 0
+        Me.LabelUsername.Text = "Username"
+        '
+        'LabelPassword
+        '
+        Me.LabelPassword.AutoSize = True
+        Me.LabelPassword.Location = New System.Drawing.Point(50, 100)
+        Me.LabelPassword.Name = "LabelPassword"
+        Me.LabelPassword.Size = New System.Drawing.Size(70, 20)
+        Me.LabelPassword.TabIndex = 1
+        Me.LabelPassword.Text = "Password"
+        '
+        'TextBoxUsername
+        '
+        Me.TextBoxUsername.Location = New System.Drawing.Point(150, 50)
+        Me.TextBoxUsername.Name = "TextBoxUsername"
+        Me.TextBoxUsername.Size = New System.Drawing.Size(125, 27)
+        Me.TextBoxUsername.TabIndex = 0
+        '
+        'TextBoxPassword
+        '
+        Me.TextBoxPassword.Location = New System.Drawing.Point(150, 100)
+        Me.TextBoxPassword.Name = "TextBoxPassword"
+        Me.TextBoxPassword.Size = New System.Drawing.Size(125, 27)
+        Me.TextBoxPassword.TabIndex = 1
+        '
+        'ButtonLogin
+        '
+        Me.ButtonLogin.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ButtonLogin.Location = New System.Drawing.Point(0, 141)
+        Me.ButtonLogin.Name = "ButtonLogin"
+        Me.ButtonLogin.Size = New System.Drawing.Size(320, 29)
+        Me.ButtonLogin.TabIndex = 2
+        Me.ButtonLogin.Text = "Login"
+        Me.ButtonLogin.UseVisualStyleBackColor = True
+        '
+        'FormLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "FormLogin"
+        Me.ClientSize = New System.Drawing.Size(320, 170)
+        Me.Controls.Add(Me.ButtonLogin)
+        Me.Controls.Add(Me.TextBoxPassword)
+        Me.Controls.Add(Me.TextBoxUsername)
+        Me.Controls.Add(Me.LabelPassword)
+        Me.Controls.Add(Me.LabelUsername)
+        Me.Name = "FormLogin"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents LabelUsername As Label
+    Friend WithEvents LabelPassword As Label
+    Friend WithEvents TextBoxUsername As TextBox
+    Friend WithEvents TextBoxPassword As TextBox
+    Friend WithEvents ButtonLogin As Button
 End Class
