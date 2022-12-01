843e3893d415a5b8c7edcd6c3ac92a8eeb7b34d3
promecarus <haikalslipi@gmail.com>
2022-12-01 15:49:24 +0700

    Update FormHapusKoleksi.vb

--- AplikasiPerpustakaan/FormHapusKoleksi.vb
+++ AplikasiPerpustakaan/FormHapusKoleksi.vb
@@ -1,4 +1,11 @@
 ﻿Public Class FormHapusKoleksi
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        LabelKoleksi.Text = FormPerpustakaan.selectedTableKoleksiNama
+    End Sub
     Private Sub ButtonHapus_Click(sender As Object, e As EventArgs) Handles ButtonHapus.Click
         'MessageBox.Show(LabelKoleksi.Text.ToString)
         FormPerpustakaan.ListBoxKoleksi.Items.Remove(LabelKoleksi.Text.ToString)
