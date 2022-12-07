04025e74da6156171abb31badab9cf1093b2146e
promecarus <haikalslipi@gmail.com>
2022-12-07 08:38:27 +0700

    Update login

--- AplikasiPerpustakaan/ClassUsers.vb
+++ AplikasiPerpustakaan/ClassUsers.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Security.Cryptography
+Imports MySql.Data.MySqlClient
 
 Public Class ClassUsers
     Private TripleDes As New TripleDESCryptoServiceProvider
@@ -6,6 +7,16 @@ Public Class ClassUsers
 
     Private username As String
     Private password As String
+
+    Public Shared dbConn As New MySqlConnection
+    Public Shared sqlCommand As New MySqlCommand
+    Public Shared sqlRead As MySqlDataReader
+    Private sqlQuery As String
+
+    Private server As String = "localhost"
+    Private username_db As String = "root"
+    Private password_db As String = ""
+    Private database As String = "db_perpustakaan"
 
     Public Sub New()
         user.Add({"admin", EncryptData("admin")})
@@ -54,5 +65,47 @@ Public Class ClassUsers
 
     Public Function AddAuth(username As String, password As String) As String
         user.Add({username, EncryptData(password)})
+    End Function
+
+    Public Function EncryptMD5(ByVal password As String)
+        Dim x As New System.Security.Cryptography.MD5CryptoServiceProvider()
+        Dim bs As Byte() = System.Text.Encoding.UTF8.GetBytes(password)
+        bs = x.ComputeHash(bs)
+        Dim s As New System.Text.StringBuilder()
+        For Each b As Byte In bs
+            s.Append(b.ToString("x2").ToLower())
+        Next
+        Return s.ToString
+    End Function
+
+    Public Function CheckAuthDatabase(username_login As String, password_login As String) As List(Of String)
+        Try
+            Dim result As New List(Of String)
+
+            dbConn.ConnectionString = "server=" + server + ";" + "user id=" + username_db + ";" + "password=" + password_db + ";" + "database=" + database
+            dbConn.Open()
+            sqlCommand.Connection = dbConn
+
+            Dim queryAuth = "SELECT id_user, username FROM users WHERE username='" & username_login & "' AND password='" & EncryptMD5(password_login) & "'"
+
+            sqlCommand.CommandText = queryAuth
+            Debug.WriteLine(queryAuth)
+            sqlRead = sqlCommand.ExecuteReader
+
+            If sqlRead.HasRows Then
+                While sqlRead.Read
+                    result.Add(sqlRead.GetString(0).ToString)
+                    result.Add(sqlRead.GetString(1).ToString)
+                End While
+            End If
+
+            sqlRead.Close()
+            dbConn.Close()
+            Return result
+        Catch ex As Exception
+            Debug.WriteLine(ex.Message)
+        Finally
+            dbConn.Dispose()
+        End Try
     End Function
 End Class
--- AplikasiPerpustakaan/FormLogin.vb
+++ AplikasiPerpustakaan/FormLogin.vb
@@ -15,16 +15,15 @@
         Dim plainUsername As String = TextBoxUsername.Text
         Dim plainPassword As String = TextBoxPassword.Text
 
-        Try
-            If Users.CheckAuth(plainUsername, plainPassword) Then
-                Me.Hide()
-                FormPerpustakaan.Show()
-            Else
-                MessageBox.Show("Password Salah")
-            End If
-        Catch ex As Exception
-            MessageBox.Show("Anda Belum Terdaftar")
-        End Try
+        Dim data_user As List(Of String) = Users.CheckAuthDatabase(plainUsername, plainPassword)
+
+        If data_user.Count > 0 Then
+            Users.GSUsername = data_user(1)
+            FormPerpustakaan.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Salah Password")
+        End If
     End Sub
 
     Private Sub LinkLabelRegister_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabelRegister.LinkClicked
